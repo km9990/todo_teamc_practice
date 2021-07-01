99c2f3ea11fdcf5a7f72f2ed4588fee2e9a673b5
ikemi hiroshi <hiroshi.ikemi@gmail.com>
2021-07-01 23:50:42 +0900

    second commit

--- store.php
+++ store.php
@@ -1,11 +1,16 @@
 <?php
+
+//ファイルの読み込み
 require_once('Models/Task.php');
 
+// データの受け取り
 $title = $_POST['title'];
 $contents = $_POST['contents'];
 $currentTime = date("Y/m/d H:i:s");
 
-$task = new task();
+// DBへのデータ保存
+$task = new Task();
 $task->create([$title, $contents, $currentTime]);
 
+// リダイレクト
 header('location:index.php');
