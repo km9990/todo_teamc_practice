69bb4ce74c6dc5cd4e9288e807929058fcaee4b3
ikemi hiroshi <hiroshi.ikemi@gmail.com>
2021-07-01 23:43:46 +0900

    first commit

--- create.php
+++ create.php
@@ -9,6 +9,34 @@
     <link rel="stylesheet" href="./style.css">
 </head>
 <body>
+    <div class="container-fuild">
+        <div class="row">
+            <nav class="navbar navbar-dark bg-dark">
+                <a href="index.php" class="navbar-brand">Todo</a>
+        </div>
+    </div>
+
+    <div class="row mt-4 px-4">
+        <div class="col-12">
+            <form action="store.php" method="post">
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" name="title" id="title">
+                </div>
+                <div class="form-group">
+                    <label for="contents">Contents</label>
+                    <textarea class="form-control" name="contents" id="contents" cols="30" rows="10"></textarea>
+                </div>
+                <div class="custom-file">
+                    <input type="file" class="custom-file-input" id="image">
+                    <label class="custom-file-label" for="image">Choose file</label>
+                </div>
+                <div class="d-flex justify-content-end">
+                    <button type="submit" class="btn btn-primary">POST</button>
+                </div>
+            </form>
+        </div>
+    </div>
 
 
 </body>
