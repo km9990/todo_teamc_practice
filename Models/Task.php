4b026ae56136126abf6babd001599017ad58be79
ikemi hiroshi <hiroshi.ikemi@gmail.com>
2021-07-06 14:04:20 +0900

    fifth commit updateメソッドの削除

--- Models/Task.php
+++ Models/Task.php
@@ -14,21 +14,4 @@ class Task extends Model
         $stmt = $this->db_manager->dbh->prepare('INSERT INTO ' . $this->table . ' (title, contents, created) VALUES (?, ?, ?)');
         $stmt->execute($data);
     }
-
-    // タスクを更新するメソッド
-    public function update($data)
-    {
-        // 実行するSQL
-        $stmt = $this->db_manager->dbh->prepare('UPDATE ' . $this->table . ' SET title = ?, contents = ? WHERE id = ?');
-        $stmt->execute($data);
-    }
-        // タイトルを元に検索する
-        public function findByTitle($data)
-        {
-            $stmt = $this->db_manager->dbh->prepare('SELECT * FROM ' . $this->table . ' WHERE title LIKE ?');
-            $stmt->execute($data);
-            $tasks = $stmt->fetchAll();
-            return $tasks;
-        }
-
 }
