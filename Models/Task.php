928ad5e2b1f1ce46f79d21e7d61ff4353e6b3187
ikemi hiroshi <hiroshi.ikemi@gmail.com>
2021-07-06 13:45:22 +0900

    forth commit Task.phpの修正

--- Models/Task.php
+++ Models/Task.php
@@ -8,21 +8,27 @@ class Task extends Model
     protected $table = 'tasks';
 
     // タスク作成のメソッド
-    public function create()
+    public function create($data)
     {
         // 実行するSQL
         $stmt = $this->db_manager->dbh->prepare('INSERT INTO ' . $this->table . ' (title, contents, created) VALUES (?, ?, ?)');
-
-
+        $stmt->execute($data);
     }
 
     // タスクを更新するメソッド
-    public function update()
+    public function update($data)
     {
         // 実行するSQL
         $stmt = $this->db_manager->dbh->prepare('UPDATE ' . $this->table . ' SET title = ?, contents = ? WHERE id = ?');
-
-
+        $stmt->execute($data);
     }
+        // タイトルを元に検索する
+        public function findByTitle($data)
+        {
+            $stmt = $this->db_manager->dbh->prepare('SELECT * FROM ' . $this->table . ' WHERE title LIKE ?');
+            $stmt->execute($data);
+            $tasks = $stmt->fetchAll();
+            return $tasks;
+        }
 
 }
