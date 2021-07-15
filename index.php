1cfb1b1e5f1186885e711caff6cf27176bdd56d5
km9990 <keito0760@gmail.com>
2021-07-15 23:21:59 +0900

    comit

--- index.php
+++ index.php
@@ -19,6 +19,11 @@ $tasks = $task->getAll();
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Todoアプリ</title>
     <link rel="stylesheet" href="./style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plaster&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Chathura&display=swap" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
  <head>
 
 
@@ -26,7 +31,7 @@ $tasks = $task->getAll();
   <div class="container-fulid">
     <header>
         <div class="header-left">
-            <a href="./index.php" class="headline">Todo_team</a>
+            <a href="./index.php" class="headline">Todo_teamC</a>
         </div>
         <div class="header-right">
             <ul class="header-content">
@@ -42,7 +47,9 @@ $tasks = $task->getAll();
                 <li class="nav-item">
                     <form class="form-inline">
                         <input class="form" type="search" placeholder="Search" aria-label="Search" name="title">
-                        <button class="btn-outline" type="submit">Search</button>
+                        <button class="btn-outline" type="submit">
+                        <span class="icon"><i class="fa fa-search"></i></span>
+                        </button>
                     </form>
                 </li>
             </ul>
@@ -55,11 +62,13 @@ $tasks = $task->getAll();
             <div class="main-content-card">
                 <img src="#" class="card-img" alt="...">
                 <div class="card-body">
+                    <div class="card-content">
                     <h5 class="card-title"> <?= $task["title"]; ?> </h5>
                     
                     <p class="card-text">
                         <?= $task["contents"];?>
                     </p>
+                    </div>
                     <div class="card-text-right">
                         <a href="edit.php?id=<?= $task['id']; ?>" class="btn text-success">EDIT</a>
                         <form action="delete.php" method="post">
